43bedc43c464234a233abf921423c2750b562149
Juan José Giménez Rejala <xymenez92@gmail.com>
2023-08-29 21:22:57 -0400

    Initial Commit

---
+++ src/Persona.java
@@ -0,0 +1,13 @@
+public class Persona {
+    String cedula;
+    String nombre;
+    String sexo;
+    int edad;
+
+    public Persona(){
+        cedula = "55555";
+        nombre = "Juan Perez";
+        sexo = "Masculino";
+        edad = 60;
+    }
+}
---
+++ src/Principal.java
@@ -0,0 +1,9 @@
+public class Principal {
+    public static void main(String[] args) {
+        Persona p = new Persona();
+        System.out.println(p.cedula);
+        System.out.println(p.nombre);
+        System.out.println(p.sexo);
+        System.out.println(p.edad);
+    }
+}
